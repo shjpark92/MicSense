bbdad81331a7b89944d46dc680ff04ae36066590
Sung Park <shjpark92@gmail.com>
2018-02-06 00:45:59 -0600

    BGM works

--- src/voice_fusion.cpp
+++ src/voice_fusion.cpp
@@ -12,8 +12,11 @@
 #include <sndfile.h>
 #include <portaudio.h>
 
-#define SAMPLE_RATE 44100
-#define FRAMES_PER_CALLBACK 2048
+//#define SAMPLE_RATE 44100
+//#define SAMPLE_RATE 33000 
+#define SAMPLE_RATE 22000
+#define FRAMES_PER_CALLBACK 4096 
+#define BUFFER_LEN 1024 
 
 #define INPUT_DEVICE Pa_GetDefaultInputDevice()
 #define INPUT_FORMAT paFloat32
@@ -26,12 +29,15 @@ typedef float OUTPUT_SAMPLE;
 using namespace std;
 
 typedef struct ioData {
+    int isInputInterleaved;
+    int isOutputInterleaved;
     int numInputChannels;
     int numOutputChannels;
     int framesPerCallback;
     int numCallbacks;
     SNDFILE *wavFile;
     SF_INFO sfinfo;
+
 } ioData;
 
 static int io_callback( const void *inputBuffer, void *outputBuffer,
@@ -46,7 +52,6 @@ static int io_callback( const void *inputBuffer, void *outputBuffer,
     short inputDone = 0, outputDone = 0;
     short finished = 0;
     unsigned int sample;
-    //sf_count_t readCounter; // sf_count_t can be replaced with an int 
     
     config->numCallbacks += 1;
     cout << timeInfo->currentTime << endl;
@@ -55,7 +60,7 @@ static int io_callback( const void *inputBuffer, void *outputBuffer,
         return finished;
     }
 
-    while (!(inputDone && outputDone)) {
+    /*while (!(inputDone && outputDone)) {
         in = ((INPUT_SAMPLE**)inputBuffer)[inChannel];
         out = ((OUTPUT_SAMPLE**)outputBuffer)[outChannel];
         
@@ -77,14 +82,19 @@ static int io_callback( const void *inputBuffer, void *outputBuffer,
         }
 
     }
-    /*memset(out, 0, sizeof(float) * framesPerBuffer * config->numOutputChannels);
-    readCounter = sf_read_float(config->wavFile, out, framesPerBuffer * config->numOutputChannels);
 
-    if (readCounter < framesPerBuffer) {
-        return paComplete;
-    }
+    return finished;
     */
-    return finished; 
+    out = ((OUTPUT_SAMPLE**)outputBuffer)[outChannel];
+    
+    memset(out, 0x00, framesPerBuffer);
+    long readcount = 0;
+    readcount = sf_read_float(config->wavFile, out, framesPerBuffer * 2);
+    if (readcount <= 0) {
+        cout << "File ended" << endl;
+        return paComplete;
+    } 
+    return paContinue;
 }
 
 int main(void) {
@@ -111,15 +121,16 @@ int main(void) {
     cout << "Output Device = " << OUTPUT_DEVICE << endl;
 
     /* libsndfile setup */
-    if(!(config->wavFile = sf_open("input.wav", SFM_READ, &config->sfinfo))) {
+    if(!(config->wavFile = sf_open("../wav/a2002011001-e02-16kHz.wav", SFM_READ, &config->sfinfo))) {
         cout << "Could not open input.wav" << endl;
         sf_perror(NULL);
         return 1;
     }
 
+
     /* Define config */
-    //config->isInputInterleaved = 0;
-    //config->isOutputInterleaved = 0;
+    config->isInputInterleaved = 0;
+    config->isOutputInterleaved = 0;
     config->framesPerCallback = FRAMES_PER_CALLBACK;
     config->numInputChannels = 2; //inputParam.channelCount;
     config->numOutputChannels = 2; //outputParam.channelCount;
@@ -139,7 +150,7 @@ int main(void) {
     fflush(stdout);
     //inputParam.channelCount = inputDeviceInfo->maxInputChannels;
     inputParam.channelCount = config->numInputChannels;
-    inputParam.sampleFormat = INPUT_FORMAT | paNonInterleaved;
+    inputParam.sampleFormat = INPUT_FORMAT | (config->isInputInterleaved ? 0 : paNonInterleaved);
     inputParam.suggestedLatency = Pa_GetDeviceInfo(inputParam.device)->defaultLowInputLatency;
     inputParam.hostApiSpecificStreamInfo = NULL;
 
@@ -157,8 +168,9 @@ int main(void) {
     fflush(stdout);
     //outputParam.channelCount = outputDeviceInfo->maxOutputChannels;
     outputParam.channelCount = config->numOutputChannels;
-    outputParam.sampleFormat = OUTPUT_FORMAT | paNonInterleaved;
+    outputParam.sampleFormat = OUTPUT_FORMAT | (config->isOutputInterleaved ? 0 : paNonInterleaved);
     outputParam.suggestedLatency = Pa_GetDeviceInfo(outputParam.device)->defaultLowOutputLatency;
+    //outputParam.suggestedLatency = Pa_GetDeviceInfo(outputParam.device)->defaultHighOutputLatency;
     outputParam.hostApiSpecificStreamInfo = NULL;
 
     /* Open stream and start */
